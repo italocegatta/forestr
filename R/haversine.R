6c455a5056feab70ca752c05928ab7460f13da29
italocegatta <italocegatta@gmail.com>
2017-04-30 13:46:36 -0300

    funs haversine, idw

---
+++ R/haversine.R
@@ -0,0 +1,24 @@
+#' Haversine distance
+#'
+#' Haversine distance between two points given their longitude and latitude
+#'
+#' @export
+#'
+haversine <- function(lon1, lat1, lon2, lat2) {
+  rad <- pi/180
+  R <- 6371000
+
+  dlon <- (lon2 - lon1) * rad
+  dlat <- (lat2 - lat1) * rad
+
+  a <- (sin(dlat/2))^2 +
+       cos(lat1 * rad) *
+       cos(lat2 * rad) *
+       (sin(dlon/2))^2
+
+  c <- 2 * atan2(sqrt(a), sqrt(1 - a))
+
+  d <- R * c * 1000
+
+  return(d)
+}
---
+++ R/idw.R
@@ -0,0 +1,9 @@
+#' Inverse distance weighting
+#'
+#'
+idw <- function(x, dist, na.rm = TRUE) {
+  s1 <-  sum(x / dist, na.rm = na.rm)
+  s2 <-  sum(dist^-1, na.rm = na.rm)
+
+  return(s1 / s2)
+}

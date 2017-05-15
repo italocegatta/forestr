9803f8ebb9fef34782e36c55f646da88cef283de
italocegatta <italocegatta@gmail.com>
2017-05-15 11:05:08 -0300

    idw typo

--- R/idw.R
+++ R/idw.R
@@ -3,8 +3,8 @@
 #' @export
 #'
 idw <- function(x, dist, na.rm = TRUE) {
-  s1 <-  sum(x / dist, na.rm = na.rm)
-  s2 <-  sum(dist^-1, na.rm = na.rm)
+  s1 <-  sum(x / dist^2, na.rm = na.rm)
+  s2 <-  sum(1 / dist^2, na.rm = na.rm)
 
   return(s1 / s2)
 }

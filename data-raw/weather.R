7be89bec0ba558429add3fa53c5e7e584171c527
italocegatta <italocegatta@gmail.com>
2017-09-14 08:32:06 -0300

    upd dataset

--- data-raw/weather.R
+++ data-raw/weather.R
@@ -3,8 +3,9 @@ library(lubridate)
 library(inmetdown)
 
 stations_id <- c("A725", "A708", "A714", "A716", "A726", "A711", "A740", "A713", "A842", "A821", "A763")
-stations <- inmetdown::aws_stations() %>%
+stations <- inmetdown::aws_station() %>%
   filter(id %in% stations_id)
+
 
 weather <- inmetdown::aws_import(
   stations_id,
@@ -12,13 +13,7 @@ weather <- inmetdown::aws_import(
   "10/07/2017"
   ) %>%
   left_join(stations, by = "id") %>%
-  mutate(
-    time = hour(date),
-    date = as.Date(date)
-  ) %>%
-  select(id, city:alt, date, time, t_max:prec) %>%
-  arrange(id, date, time)
+  select(id, city:alt, date, hour, t_min, rh_min, prec) %>%
+  arrange(id, date, hour)
 
 devtools::use_data(weather, overwrite = TRUE)
-
-

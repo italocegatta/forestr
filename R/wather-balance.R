9827d57367c9fd13cf6386a9897db8d29876dfc2
italocegatta <italocegatta@gmail.com>
2017-12-19 15:05:13 -0200

    typo

--- R/wather-balance.R
+++ R/wather-balance.R
@@ -1,19 +1,15 @@
-#' @export
 rad <- function(x) {
   pi * x / 180
 }
 
-#' @export
 delta <- function(doy){
   23.45 * sin(rad(360 * (doy - 81) / 365))
 }
 
-#' @export
 hn <- function(lat, delta) {
   acos(-tan(rad(lat)) * tan(rad(delta))) * 180 / pi
 }
 
-#' @export
 n_days <- function(x) {
 
   last <- seq.Date(max(x), max(x) + 32, by = "month")[2]
@@ -21,7 +17,6 @@ n_days <- function(x) {
   as.numeric(diff(c(x, last)))
 }
 
-#' @export
 n_hours <- function(date, lat) {
 
   delta <- delta(doy(date))
@@ -40,20 +35,24 @@ wb_a <- function(i){
   0.49 + 0.018 * i - 7.7 * (10^-5) * (i^2) + 6.75 * (10^-7) * (i^3)
 }
 
-
 #' @export
-etp <- function(date, temp, lat) {
+etp <- function(date, t_med, lat, i = NULL, a = NULL) {
 
-  i = wb_i(temp)
-  a = wb_a(i)
+  if (is.null(i)) {
+    i <- wb_i(t_med)
+  }
+
+  if (is.null(a)) {
+    a <- wb_a(i)
+  }
 
   n_days <- n_days(date)
   n_hours <- n_hours(date, lat)
 
   ifelse(  # precisa vetorizar?
-    temp < 26.5,
-    16 * ((10 * (temp / i))^a) * (n_hours / 12) * (n_days / 30),
-    (-415.85 + 32.24 * temp - 0.43 * temp^2) * (n_hours / 12) * (n_days/30)
+    t_med < 26.5,
+    16 * ((10 * (t_med / i))^a) * (n_hours / 12) * (n_days / 30),
+    (-415.85 + 32.24 * t_med - 0.43 * t_med^2) * (n_hours / 12) * (n_days/30)
   )
 }
 
@@ -62,20 +61,15 @@ etp <- function(date, temp, lat) {
 # prec = "prec"
 # etp = "etp"
 
-# wb <- function(.data, cad, prec, etp) {
-#
-#   prec <- dplyr::enquo(prec)
-#   etp <- dplyr::enquo(etp)
-#   cad <- dplyr::enquo(cad)
-#
-#   p_etp <- pull(.data, !!prec) - pull(.data, !!etp)
-#
-#   guess_start <- 4
-#
-#   neg_acum <-
-#
-#
-# }
+wb <- function(.data, cad, prec, etp) {
+
+  prec <- dplyr::enquo(prec)
+  etp <- dplyr::enquo(etp)
+  cad <- dplyr::enquo(cad)
+
+  p_etp <- pull(.data, !!prec) - pull(.data, !!etp)
+
+}
 
 # x <- p_etp
 # i = 1
@@ -84,11 +78,12 @@ guess_start <- function(x) {
   #n = length(x)
 
   for (i in seq_along(x)) {
-   all(x[0:2 + i] > 0)
+   guess[i] <- all(x[0:2 + i] > 0)
   }
 
-}
+  which(x, guess[1])
 
+}
 
 seq_cycle <- function(x, new) {
 
--- data-raw/debug-wb.R
+++ data-raw/debug-wb.R
@@ -1,13 +1,34 @@
 library(tidyverse)
 library(forestr)
 
-df <- data_frame(
-  date = seq.Date(as.Date("2017-01-01"), as.Date("2017-12-01"), "month"),
-  cad = 100,
-  lat = -22.93,
-  t_med = c(23.10, 23.50, 23.00, 21.10, 18.70, 17.40, 17.30, 18.90, 20.10, 21.20, 22.00, 22.50),
-  prec =  c(240.20, 190.90, 147.30, 71.00, 65.10, 48.70, 36.80, 37.40, 65.60, 123.60, 137.50, 217.10)
-  )
+
+date = seq.Date(as.Date("2017-01-01"), as.Date("2017-12-01"), "month")
+cad = 100
+lat = -22.93
+t_med = c(23.10, 23.50, 23.00, 21.10, 18.70, 17.40, 17.30, 18.90, 20.10, 21.20, 22.00, 22.50)
+prec =  c(240.20, 190.90, 147.30, 71.00, 65.10, 48.70, 36.80, 37.40, 65.60, 123.60, 137.50, 217.10)
+i = 111 # 103.7919
+a = 2.5 # 2.283487
+
+df <- data_frame(date, cad, lat, t_med, prec, i, a)
+
+base <- df %>%
+  mutate(etp = round(etp(date, t_med, lat, 111, 2.5), 1)) %>%
+  select(date, prec, etp)
+
+etp <- base$etp
+
+base %>%
+  mutate(p_etp = prec - etp)
+
+mutate(df, id = 1) %>%
+  bind_rows(mutate(data_frame(date, cad, lat, t_med, prec, i = 103.7919, a = 2.283487), id = 2)) %>%
+  group_by(id) %>%
+  mutate(etp = round(etp(date, t_med, lat, i, a), 1)) %>%
+  View()
+
+
+round(16 * ((10 * (t_med / i))^a) * (n_hours / 12) * (n_days / 30), 1)
 
 df3 <- bind_rows(df, df, .id = "id")
 

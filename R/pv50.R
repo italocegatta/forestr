6cdf9c35d7acbdc7f2228b8e617103b364be27f9
italocegatta <italocegatta@gmail.com>
2018-02-03 10:52:23 -0300

    mudanca na definicao posicao 50%

--- R/pv50.R
+++ R/pv50.R
@@ -8,16 +8,15 @@ pv50 <- function(x) {
 
   n50 <- length(x)/2
 
+  # para 5 arv, pega a 3 posicao
+  # para 6 arv, pega a 3 posicao
+  if (n50%%2L != 1L) {
+    n50 <- ceiling(n50)
+  }
+
   sum <- sum(x, na.rm = TRUE)
 
   cum_sum <- c(n_na, cumsum(sort(x)))
 
-  if (n50%%2L == 1L)
-    sum50 <- mean(cum_sum[n50], na.rm = TRUE)
-  else
-    sum50 <- mean(cum_sum[n50 + 0L:1L], na.rm = TRUE)
-
-  z <- sum50 / sum * 100
-
-  return(z)
+  cum_sum[n50] / sum * 100
 }

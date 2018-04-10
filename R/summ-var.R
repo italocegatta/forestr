0b8987324e02fb39d4f268cedbd28fa680b32c90
italocegatta <italocegatta@gmail.com>
2018-04-10 11:16:26 -0300

    speed up summ-var-part

--- R/summ-var.R
+++ R/summ-var.R
@@ -17,13 +17,22 @@ summarise_var <- function(vec, fun = mean, n_min = 18, ...) {
 #' @export
 #'
 summarise_var_part <- function(key, vec, fun = mean, groups = 3, n_min = 6, ...) {
-  vec_cut <- split(vec, cut(key, breaks = groups))
-
-  vec_fun <- purrr::map_dbl(vec_cut, ~summarise_var(.x, fun, n_min))
-
-  if (!all(!is.na(vec_fun))) {
-    return(NA_real_)
+  nb <- as.integer(groups + 1)
+  dx <- diff(rx <- range(key, na.rm = TRUE))
+  if (dx == 0) {
+    dx <- abs(rx[1L])
+    breaks <- seq.int(rx[1L] - dx/1000, rx[2L] + dx/1000, length.out = nb)
+  } else {
+    breaks <- seq.int(rx[1L], rx[2L], length.out = nb)
   }
 
-  fun(vec_fun, ...)
+  vec_cut <- split(vec, .bincode(key, breaks, TRUE, TRUE))
+  #vec_na <- purrr::map_dbl(vec_cut, ~sum(!is.na(.x)))
+  vec_na <- sapply(vec_cut, function(x) sum(!is.na(x)), simplify = TRUE)
+
+  if (any(vec_na < n_min)) {
+    return(NA_real_)
+  } else {
+    fun(vec, ...)
+  }
 }
